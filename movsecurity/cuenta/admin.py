f0f9242f052acc73d7209af595cb622b77ac200c
CapitanCrauZ <paologhost14.pr@gmail.com>
2022-07-08 19:59:30 -0400

    rewatching

--- movsecurity/cuenta/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import TipoUsuario,PerfilUsuario
-
-admin.site.register(TipoUsuario)
-admin.site.register(PerfilUsuario)
-
-# Register your models here.
--- movsecurity/productos/models.py
+++
@@ -1,8 +0,0 @@
-from djongo import models
-
-# Create your models here.
-
-class Productos(models.Model):
-    nombre= models.CharField(max_length=45, blank=False)
-    descripcion = models.CharField(max_length=45, blank=False)
-    cantidad = models.IntegerField(blank=False)
